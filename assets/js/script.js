c6c24be918d7b64e71f191624629070080fcf830
Eugene Leong <honghao@hotmail.sg>
2019-12-06 16:17:41 +1100

    script edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,37 @@
 
 $("#currentDateTime").text(moment().format('dddd, DD MMMM YYYY, h:mm a'))
 
-console.log(moment().format('ha'))
+console.log(moment().format('hA'))
 
-var currentTime = (moment().format('ha'))
+var currentTime = (moment().format('hA'))
 
 console.log(currentTime)
 
-console.log($(".time"))
+var findTime = $(".time")
+var searchTime = currentTime;
+var found;
 
-if ("8pm" == currentTime) {
-    $("td").css({"background-color": "#ffe"});
+console.log(searchTime);
+
+for (var i = 0; i < findTime.length; i++) {
+  if (findTime[i].textContent == searchTime) {
+    found = findTime[i];
+    break;
+  }
 }
 
+console.log(found);
 
+var changeColorTd = found.nextSibling.nextSibling;
+var changeColorTextArea =  found.nextSibling.nextSibling.children[0]
+
+
+changeColorTd.className += " tableColorNow";
+changeColorTextArea.className += " tableColorNow";
+// if ("2am" == currentTime) {
+//     $("textarea").addClass("tableColorNow")
+//     $(".textBox").addClass("tableColorNow")
+// }
 
 // color of table that is same time as current time will be green
 
